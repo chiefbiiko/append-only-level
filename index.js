3a0d7ead37e4587235d2294b6351a3ff42221baf
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-04-15 09:38:53 +0200

    debug strategy

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 var { Readable, Writable } = require('stream')
 var debug = require('debug')('append-only-level')
 
-// TODO: get db's max key 4 _head @ init or -1 
+// TODO: get db's max key 4 _head @ init or -1
+// find a way to get db size at init, maybe mafintosh/end-of-stream
+
 function Log (db) {
   if (!(this instanceof Log)) return new Log(db)
   else if (!db) throw TypeError('levelup instance required')
   this._db = db
   this._head = -1
-  //
-  this._choking = true
-  this._waiting = []
-  var keystream = this._db.createKeyStream()
-  keystream.on('data', function () {})
-  keystream.on('end', function () {})
-  //
+
+  // this._choking = true
+  // this._waiting = []
+  // var self = this
+  // var keystream = this._db.createKeyStream()
+  // keystream.on('data', function (key) {
+  //   debug('key::', key)
+  //   self._head = key
+  // })
+  // keystream.on('end', function () {
+  //   while (self._waiting.length) self.append(self._waiting.shift())
+  //   self._choking = false
+  // })
+
 }
 
 function count () {
@@ -24,9 +33,9 @@ Log.prototype.__defineGetter__('size', count)
 Log.prototype.__defineGetter__('length', count)
 
 Log.prototype.append = function append (value, cb) {
-  //
-  if (this._choking) return this._waiting.push(value)
-  //
+
+  // if (this._choking) return this._waiting.push(value)
+
   var self = this
   self._db.put(String(++self._head), value, function (err) {
     if (err) return cb.call(self, err)
